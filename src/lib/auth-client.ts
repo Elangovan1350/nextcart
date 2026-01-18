ed478d532792b22f7c9217e0e02f1b39f68cabce
Elangovan1350 <elangovan2019miss@gmail.com>
2026-01-18 20:05:55 +0530

    change4

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import { createAuthClient } from "better-auth/react";
 export const { signIn, signUp, useSession, signOut, getSession } =
   createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL,
   });
